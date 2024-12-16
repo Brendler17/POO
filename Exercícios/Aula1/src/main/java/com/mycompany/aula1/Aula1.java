11fbc15e2e2230eb369315369dba8695115a7141
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-12-16 20:05:35 -0300

    added Aula1 main

---
+++ Exercícios/Aula1/src/main/java/com/mycompany/aula1/Aula1.java
@@ -0,0 +1,14 @@
+package com.mycompany.aula1;
+
+public class Aula1 {
+
+    public static void main(String[] args) {
+        //Exercise1.executar();
+        //Exercise2.executar();
+        //Exercise3.executar();
+        //Exercise4.executar();
+        //Exercise5.executar();
+        //Exercise6.executar();
+        Exercise7.executar();
+    }
+}
