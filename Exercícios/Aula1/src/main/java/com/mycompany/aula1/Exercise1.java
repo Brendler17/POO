d2fe2f7560a8f58120456eae3c7ba4cb777c2d0b
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2024-12-16 20:06:29 -0300

    added exercise1

---
+++ Exercícios/Aula1/src/main/java/com/mycompany/aula1/Exercise1.java
@@ -0,0 +1,23 @@
+package com.mycompany.aula1;
+
+public class Exercise1 {
+
+    public static void executar() {
+        System.out.println("Executando o exercício 1: \n");
+
+        for (int counter = 1; counter <= 10; counter++) {
+            System.out.println("Taboada do " + counter);
+            System.out.println(1 + "x" + counter + " = " + 1 * counter);
+            System.out.println(2 + "x" + counter + " = " + 2 * counter);
+            System.out.println(3 + "x" + counter + " = " + 3 * counter);
+            System.out.println(4 + "x" + counter + " = " + 4 * counter);
+            System.out.println(5 + "x" + counter + " = " + 5 * counter);
+            System.out.println(6 + "x" + counter + " = " + 6 * counter);
+            System.out.println(7 + "x" + counter + " = " + 7 * counter);
+            System.out.println(8 + "x" + counter + " = " + 8 * counter);
+            System.out.println(9 + "x" + counter + " = " + 9 * counter);
+            System.out.println(10 + "x" + counter + " = " + 10 * counter);
+            System.out.println("\n");
+        }
+    }
+}
