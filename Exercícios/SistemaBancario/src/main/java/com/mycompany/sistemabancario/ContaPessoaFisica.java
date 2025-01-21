05c2eeedec3da32365c23d19e593deba29c0b227
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-01-21 18:24:49 -0300

    updated constructor

--- Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaFisica.java
+++ Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaFisica.java
@@ -5,10 +5,14 @@ public class ContaPessoaFisica extends ContaBanco {
     private final String CPF;
     private final Data birth;
 
-    public ContaPessoaFisica(String name, double balance, String CPF, Data date) {
-        super(name, balance);
+    public ContaPessoaFisica(String name, double balance, double limit, String CPF, Data date) {
+        super(name, balance, limit);
         this.CPF = CPF;
         this.birth = date;
+    }
+
+    public ContaPessoaFisica(String name, double balance, String CPF, Data date) {
+        this(name, balance, 0, CPF, date);
     }
 
     public String toString() {
