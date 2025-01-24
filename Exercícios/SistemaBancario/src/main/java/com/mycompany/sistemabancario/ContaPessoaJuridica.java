c2cbd6e9fa5b49e43ff9ae9c2fa53a91f27d8dc1
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-01-24 15:55:49 -0300

    updated class ContaPessoaJuridica

--- Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaJuridica.java
+++ Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaJuridica.java
@@ -7,12 +7,45 @@ public class ContaPessoaJuridica extends ContaBanco {
 
     public ContaPessoaJuridica(String name, double balance, double limit, String CNPJ, String stateRegistration) {
         super(name, balance, limit);
+
+        if (CNPJ == null || CNPJ.isEmpty()) {
+            throw new IllegalArgumentException("\nO CNPJ não pode ser nulo!\n");
+        }
+
+        if (stateRegistration == null || stateRegistration.isEmpty()) {
+            throw new IllegalArgumentException("\nO registro não pode ser nulo!\n");
+        }
+
         this.CNPJ = CNPJ;
         this.stateRegistration = stateRegistration;
     }
 
     public ContaPessoaJuridica(String name, double balance, String CNPJ, String stateRegistration) {
         this(name, balance, 0, CNPJ, stateRegistration);
+    }
+
+    public String getCNPJ() {
+        return this.CNPJ;
+    }
+
+    public void setCNPJ(String CNPJ) {
+        if (CNPJ == null || CNPJ.isEmpty()) {
+            throw new IllegalArgumentException("\nO CNPJ não pode ser nulo!\n");
+        }
+
+        this.CNPJ = CNPJ;
+    }
+
+    public String getStateRegistration() {
+        return this.stateRegistration;
+    }
+
+    public void setStateRegistration(String stateRegistration) {
+        if (stateRegistration == null || stateRegistration.isEmpty()) {
+            throw new IllegalArgumentException("\nO registro não pode ser nulo!\n");
+        }
+
+        this.stateRegistration = stateRegistration;
     }
 
     @Override
