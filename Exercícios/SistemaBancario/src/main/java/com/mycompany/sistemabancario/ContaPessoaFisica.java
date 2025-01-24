8ff2f39a555f4d9df0797f24d15f6173c84fd9d9
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-01-24 15:55:35 -0300

    updated class ContaPessoaFisica

--- Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaFisica.java
+++ Exercícios/SistemaBancario/src/main/java/com/mycompany/sistemabancario/ContaPessoaFisica.java
@@ -2,17 +2,34 @@ package com.mycompany.sistemabancario;
 
 public class ContaPessoaFisica extends ContaBanco {
 
-    private final String CPF;
-    private final Data birth;
+    private String CPF;
+    private Data birth;
 
     public ContaPessoaFisica(String name, double balance, double limit, String CPF, Data date) {
         super(name, balance, limit);
+
+        if (CPF == null || CPF.isEmpty()) {
+            throw new IllegalArgumentException("\nO CPF não pode ser nulo!\n");
+        }
+
         this.CPF = CPF;
         this.birth = date;
     }
 
     public ContaPessoaFisica(String name, double balance, String CPF, Data date) {
         this(name, balance, 0, CPF, date);
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+
+    public void setCPF(String CPF) {
+        if (CPF == null || CPF.isEmpty()) {
+            throw new IllegalArgumentException("\nO CPF não pode ser nulo!\n");
+        }
+
+        this.CPF = CPF;
     }
 
     public String toString() {
