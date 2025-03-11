e830d66d85f8fbb155ebec9c87719ccb95087645
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-11 15:33:05 -0300

    updated main

--- Zombicide/src/main/java/com/mycompany/zombicide/Zombicide.java
+++ Zombicide/src/main/java/com/mycompany/zombicide/Zombicide.java
@@ -3,7 +3,6 @@ package com.mycompany.zombicide;
 public class Zombicide {
 
     public static void main(String[] args) {
-        char[][] map = MapManager.loadRandomMap();
         new WelcomeScreen();
     }
 }
