861e4dfb7ca9927492e08e0cb18f0c62d6f701cc
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-12 15:19:43 -0300

    created ZombieCreeping class

---
+++ Zombicide/src/main/java/com/mycompany/zombicide/ZombieCreeping.java
@@ -0,0 +1,11 @@
+package com.mycompany.zombicide;
+
+public class ZombieCreeping extends Zombie {
+
+    public ZombieCreeping(int positionX, int positionY) {
+        super(1, positionX, positionY);
+    }
+
+    public void Move(int[] position) {
+    }
+}
