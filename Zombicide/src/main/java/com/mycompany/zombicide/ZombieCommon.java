4797d092bd2d56220fbf74c1534c092ebb2dfda2
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-12 15:19:14 -0300

    created ZombieCommon class

---
+++ Zombicide/src/main/java/com/mycompany/zombicide/ZombieCommon.java
@@ -0,0 +1,11 @@
+package com.mycompany.zombicide;
+
+public class ZombieCommon extends Zombie {
+
+    public ZombieCommon(int positionX, int positionY) {
+        super(2, positionX, positionY);
+    }
+
+    public void Move(int[] position) {
+    }
+}
