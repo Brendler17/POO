75e9b23c0663d82a57af0c4d229f93e4a2c5b6f6
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-12 15:20:32 -0300

    created ZombieRunner class

---
+++ Zombicide/src/main/java/com/mycompany/zombicide/ZombieRunner.java
@@ -0,0 +1,11 @@
+package com.mycompany.zombicide;
+
+public class ZombieRunner extends Zombie {
+
+    public ZombieRunner(int positionX, int positionY) {
+        super(2, positionX, positionY);
+    }
+
+    public void Move(int[] position) {
+    }
+}
