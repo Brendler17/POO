8a62d26a4d2802a8f17d0d2971cb6daca4311e00
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-12 15:49:35 -0300

    updated Chest class

--- Zombicide/src/main/java/com/mycompany/zombicide/Chest.java
+++ Zombicide/src/main/java/com/mycompany/zombicide/Chest.java
@@ -5,8 +5,6 @@ public class Chest {
     int[] position;
 
     public Chest(int positionX, int positionY) {
-        position = new int[2];
-        position[0] = positionX;
-        position[1] = positionY;
+        this.position = new int[]{positionX, positionY};
     }
 }
