068eb4828edc69a47e2c7349dc9de68759a47577
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-13 12:51:06 -0300

    updated Hero class

--- Zombicide/src/main/java/com/mycompany/zombicide/Hero.java
+++ Zombicide/src/main/java/com/mycompany/zombicide/Hero.java
@@ -5,20 +5,34 @@ public class Hero {
     private int health;
     private int perception;
     private int[] position;
+    private String currentWeapon;
 
     public Hero(int positionX, int positionY, int playerPerception) {
         this.health = 5;
         this.perception = playerPerception;
         this.position = new int[]{positionX, positionY};
-    }
-
-    public int[] getPosition() {
-        return position;
+        this.currentWeapon = "hands";
     }
 
     public void Move(int newX, int newY) {
     }
 
     public void Combat(Zombie zombie) {
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getPerception() {
+        return perception;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public String getCurrentWeapon() {
+        return currentWeapon;
     }
 }
