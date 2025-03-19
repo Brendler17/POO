d9d8e1cecb49089d45223b29e2b06ae10eb58932
Brendler17 <gmbrendler@inf.ufpel.edu.br>
2025-03-19 17:05:03 -0300

    created logic to assign chest contents

--- Zombicide/src/main/java/com/mycompany/zombicide/GameManager.java
+++ Zombicide/src/main/java/com/mycompany/zombicide/GameManager.java
@@ -26,6 +26,22 @@ public class GameManager {
         distributeChestContents();
     }
 
+    private void distributeChestContents() {
+        if (chests.size() != 4) {
+            System.err.println("Erro: O mapa deve conter exatamente 4 baús!");
+            return;
+        }
+
+        String[] contents = {"bandage", "baseball_bat", "revolver_zombie", "revolver_zombie"};
+
+        List<String> contentList = Arrays.asList(contents);
+        Collections.shuffle(contentList);
+
+        for (int i = 0; i < chests.size(); i++) {
+            chests.get(i).setContent(contentList.get(i));
+        }
+    }
+
     private void iniatilizeEntities(int playerPerception) {
         Zombie newZombie;
         for (int i = 0; i < mapData.length; i++) {
@@ -80,22 +96,6 @@ public class GameManager {
             mapData[newX][newY] = 'P';
             gameUI.updateUI();
             gameUI.createMovementButtons();
-        }
-    }
-
-    private void distributeChestContents() {
-        if (chests.size() != 4) {
-            System.err.println("Erro: O mapa deve conter exatamente 4 baús!");
-            return;
-        }
-
-        String[] contents = {"bandage", "baseball_bat", "revolver_zombie", "revolver_zombie"};
-
-        List<String> contentList = Arrays.asList(contents);
-        Collections.shuffle(contentList);
-
-        for (int i = 0; i < chests.size(); i++) {
-            chests.get(i).setContent(contentList.get(i));
         }
     }
 
